ba7a3bb5eb3fc8d8d990a87162d18d0f3165429f
Ferdef <f3rd3f@gmail.com>
2025-05-08 09:11:18 +0200

    Added functions' skeletons and filled in getData and getById

---
+++ backend/src/services/storageService.ts
@@ -0,0 +1,66 @@
+import fs from "fs";
+import path from "path";
+
+export async function saveData(collection: string, data: any[]): Promise<void> {
+
+}
+
+export async function getData(collection: string): Promise<any[]> {
+  try {
+    const filePath = getCollectionFile(collection);
+
+    if (!fs.existsSync(filePath)) {
+        await fs.promises.writeFile(filePath, JSON.stringify([]));
+        return [];
+    }
+
+    const data = await fs.promises.readFile(filePath, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Error getting data from ${collection}:`, error);
+    throw error;
+  }
+}
+
+export async function getById(collection: string, id: string): Promise<any | null> {
+  try {
+    const filePath = getCollectionFile(collection);
+
+    if (!fs.existsSync(filePath)) {
+      await fs.promises.writeFile(filePath, JSON.stringify([]));
+      return null;
+    }
+
+    const data = await fs.promises.readFile(filePath, 'utf8');
+    return JSON.parse(data)[id];
+  } catch (error) {
+    console.error(`Error getting data from ${collection}:`, error);
+    throw error;
+  }
+}
+
+export async function createItem(collection: string, item: any): Promise<any> {
+  try {
+
+  } catch (error) {
+    console.error(`Error getting data from ${collection}:`, error);
+    throw error;
+  }
+}
+
+export async function deleteItem(collection: string, id: string): Promise<boolean> {
+  try {
+    return false;
+  } catch (error) {
+    console.error(`Error getting data from ${collection}:`, error);
+    throw error;
+  }
+}
+
+function getCollectionFile(collection: string): string {
+  if(!['models', 'run', 'metrics'].includes(collection)) {
+    throw new Error(`Invalid Collection: ${collection}`);
+  }
+
+  return path.join(__dirname, `../../data/${collection}.json`);
+}
